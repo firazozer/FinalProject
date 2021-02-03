c85c5e6f12ea2ac6977bf45da2deff501adf9fe2
firazozer <46114609+firazozer@users.noreply.github.com>
2021-02-04 01:20:45 +0300

    EntityFrameWork Added

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    // Context : Context nesnesi Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)//Bu metot sayesınde hangı verıtabanı ıle ılıskılısın onu belırlıeyecek metot
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\projectsv13;Database=Northwind;Trusted_Connection=true");//sql server bağlantısı
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Custormers { get; set; }
+
+
+
+    }
+}
