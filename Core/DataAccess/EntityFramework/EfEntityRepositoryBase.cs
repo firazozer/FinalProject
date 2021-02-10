2beeba21ad73a61e94e84658a1dc607cf6e2143c
firazozer <46114609+firazozer@users.noreply.github.com>
2021-02-10 16:33:47 +0300

    Core Implement

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿using Core.Entitites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext>: IEntityRepository<TEntity>
+        where TEntity: class, IEntity, new()
+        where TContext: DbContext, IEntity, new()
+
+    {
+        public void Add(TEntity entity)
+        {
+            // IDisposable pattern implementation of C#
+            using (TContext conctex = new TContext())
+            {
+                var addedEntity = conctex.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                conctex.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext conctex = new TContext())
+            {
+                var deletedEntity = conctex.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                conctex.SaveChanges();
+            }
+        }
+
+        public  TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext conctex = new TContext())
+            {
+                var updatedEntity = conctex.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                conctex.SaveChanges();
+            }
+        }
+    }
+}
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -11,54 +11,6 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfProductDal : IProductDal
     {
-        public void Add(Product entity)
-        {
-            // IDisposable pattern implementation of C#
-            using (NorthwindContext conctex = new NorthwindContext())
-            {
-                var addedEntity = conctex.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                conctex.SaveChanges();
-            }
-        }
 
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext conctex = new NorthwindContext())
-            {
-                var deletedEntity = conctex.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                conctex.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter )
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-       
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter == null
-                    ? context.Set<Product>().ToList()
-                    : context.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext conctex = new NorthwindContext())
-            {
-                var updatedEntity = conctex.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                conctex.SaveChanges();
-            }
-        }
     }
 }
